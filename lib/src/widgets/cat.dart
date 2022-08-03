b36bc7f8422f88e0d1898799ca6cfe3229318dee
rahat <rahatislamakash@gmail.com>
2022-08-04 04:10:40 +0600

    learning animation

---
+++ lib/src/widgets/cat.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Cat extends StatelessWidget{
+  build(context){
+    return Image.network('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipart-library.com%2Fimages_k%2Ftransparent-cat-face%2Ftransparent-cat-face-1.png');
+  }
+}
